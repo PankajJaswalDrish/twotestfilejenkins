30af4beafbf9861b0985b35d29bce83a9fa4c29a
DR69 <pj350284.pj@gmail.com>
2021-07-06 16:48:51 +0530

    aa

--- backup.sh
+++ backup.sh
@@ -1,2 +1,4 @@
-cp -r /home/dr69/Desktop/gitDemo/test.py test1.py /home/dr69/Desktop/gitDemo/backup/
+sudo sudo 
+dri123
+sudo cp -r /home/dr69/Desktop/gitDemo/test.py test1.py /home/dr69/Desktop/gitDemo/backup/
 
