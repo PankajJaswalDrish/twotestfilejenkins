68a1b9aa21a8995482bbf587f407c3c504f472a6
DR69 <pj350284.pj@gmail.com>
2021-07-06 16:28:08 +0530

    aa

--- backup.sh
+++ backup.sh
@@ -1,4 +1,3 @@
-#Copy file from one folder to another folder for backup purpose.
 #!/bin/sh
-cp -r test.py test1.py /home/dr69/Desktop/gitDemo/backup/
+sudo cp -r test.py test1.py /home/dr69/Desktop/gitDemo/backup/
 
