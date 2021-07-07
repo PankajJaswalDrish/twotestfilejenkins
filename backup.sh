686501fc45d469fd26b26b46bcb6da27d53fa180
DR69 <pj350284.pj@gmail.com>
2021-07-07 16:20:43 +0530

    add new

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
-cp -rf /home/dr69/Desktop/gitDemo/test.py /home/dr69/Desktop/gitDemo/backup/
+sudo cp -rf /home/dr69/Desktop/gitDemo/test.py /home/dr69/Desktop/gitDemo/backup/
 #cp -r /home/dr69/Desktop/gitDemo/test1.py /home/dr69/Desktop/gitDemo/backup/
 
