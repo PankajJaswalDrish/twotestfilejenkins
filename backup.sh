b43a8fe4ccdf8e0e81e34839bef178a0d0e7c03c
DR69 <pj350284.pj@gmail.com>
2021-07-07 15:30:13 +0530

    add new

--- backup.sh
+++ backup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-sudo su -
-cp -r /home/dr69/Desktop/gitDemo/test.py /home/dr69/Desktop/gitDemo/backup/
-cp -r /home/dr69/Desktop/gitDemo/test1.py /home/dr69/Desktop/gitDemo/backup/
+cp -rf /home/dr69/Desktop/gitDemo/test.py /home/dr69/Desktop/gitDemo/backup/
+#cp -r /home/dr69/Desktop/gitDemo/test1.py /home/dr69/Desktop/gitDemo/backup/
 
